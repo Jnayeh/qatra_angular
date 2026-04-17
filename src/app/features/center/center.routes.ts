b734d5ee8e5318d5d540e014624236981ebb4ad8
Mourad Slim Jnayeh <mouradslim44@gmail.com>
2026-04-17 10:00:00 +0100

    add center routes

---
+++ src/app/features/center/center.routes.ts
@@ -0,0 +1,48 @@
+import type { Routes } from '@angular/router';
+import { roleGuard } from '@/app/core/auth/guards/role.guard';
+
+export default [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  {
+    path: 'list',
+    loadComponent: () => import('@/app/features/center/pages/center-list-page/center-list-page').then((m) => m.CenterListPageComponent),
+  },
+  {
+    path: ':id',
+    loadComponent: () => import('@/app/features/center/pages/center-detail-page/center-detail-page').then((m) => m.CenterDetailPageComponent),
+  },
+  {
+    path: ':id/manage',
+    canActivate: [roleGuard('CENTER_ADMIN', 'SUPER_ADMIN')],
+    loadComponent: () => import('@/app/features/center/pages/center-manage-page/center-manage-page').then((m) => m.CenterManagePageComponent),
+  },
+  {
+    path: ':id/slots',
+    canActivate: [roleGuard('CENTER_ADMIN', 'SUPER_ADMIN')],
+    loadComponent: () => import('@/app/features/center/pages/slot-management-page/slot-management-page').then((m) => m.SlotManagementPageComponent),
+  },
+  {
+    path: ':id/book',
+    loadComponent: () => import('@/app/features/center/pages/slot-booking-page/slot-booking-page').then((m) => m.SlotBookingPageComponent),
+  },
+  {
+    path: ':id/staff',
+    canActivate: [roleGuard('CENTER_ADMIN', 'SUPER_ADMIN')],
+    loadComponent: () => import('@/app/features/center/pages/staff-management-page/staff-management-page').then((m) => m.StaffManagementPageComponent),
+  },
+  {
+    path: ':id/activity-log',
+    canActivate: [roleGuard('CENTER_ADMIN', 'SUPER_ADMIN')],
+    loadComponent: () => import('@/app/features/center/pages/staff-activity-log-page/staff-activity-log-page').then((m) => m.StaffActivityLogPageComponent),
+  },
+  {
+    path: ':id/analytics',
+    canActivate: [roleGuard('CENTER_ADMIN', 'SUPER_ADMIN')],
+    loadComponent: () => import('@/app/features/center/pages/center-analytics-page/center-analytics-page').then((m) => m.CenterAnalyticsPageComponent),
+  },
+  {
+    path: ':id/reports',
+    canActivate: [roleGuard('CENTER_ADMIN', 'SUPER_ADMIN')],
+    loadComponent: () => import('@/app/features/center/pages/center-reports-page/center-reports-page').then((m) => m.CenterReportsPageComponent),
+  },
+] as Routes;
