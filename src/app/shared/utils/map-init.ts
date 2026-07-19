d1c7c65fc381432035dd27c60d620113b477d583
Mourad Slim Jnayeh <mourad.jnayeh@keyrus.com>
2026-07-19 22:41:12 +0100

    fix: fixes map arabic

--- src/app/shared/utils/map-init.ts
+++ src/app/shared/utils/map-init.ts
@@ -5,6 +5,10 @@ let initialized = false;
 export function initMapLibre(): void {
   if (!initialized && typeof window !== 'undefined') {
     (window as any).maplibregl = maplibregl;
+    maplibregl.setRTLTextPlugin(
+      'https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.3.0/dist/mapbox-gl-rtl-text.js',
+      true,
+    );
     initialized = true;
   }
 }
